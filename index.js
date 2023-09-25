d6e2f3fdf6152e6d0d84984799a836e5f3aa1777
Bishal9-Teesta <bishal@teesta.co>
2023-09-25 15:03:55 +0530

    File creation

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const future = require("./future")
 const option = require("./option")
 
 const main = async () => {
+
+    require("fs").writeFileSync(`Opportunity-${process.argv[2]}.csv`, "ITM Symbol, Future Symbol, Future Price, ITM Floor Price, OTM Floor Price, Total Quantity, Total Expense, Total Profit, Total Required Margin To Trade, Profit In Points, Expiry In Days, Trade ROI, Future Book, ITM Book, OTM Book", { encoding: "utf-8" })
+
     future.orderbook()
     option.orderbook()
 
